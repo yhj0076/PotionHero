324a73b74feb28c38a4e0fcf06d1855157434ac4
King0wl <70518135+yhj0076@users.noreply.github.com>
2025-04-16 22:12:39 +0900

    Create SessionManager.cs

---
+++ DummyClient/Session/SessionManager.cs
@@ -0,0 +1,33 @@
+﻿namespace DummyClient.Session;
+
+public class SessionManager
+{
+    #region Singleton
+    static SessionManager _session = new SessionManager();
+    public static SessionManager Instance { get { return _session; } }
+    #endregion
+    
+    List<ServerSession> _sessions = new List<ServerSession>();
+    object _lock = new object();
+
+    public void SendForEach()
+    {
+        lock (_lock)
+        {
+            foreach (var session in _sessions)
+            {
+                
+            }
+        }
+    }
+
+    public ServerSession Generate()
+    {
+        lock (_lock)
+        {
+            ServerSession session = new ServerSession();
+            _sessions.Add(session);
+            return session;
+        }
+    }
+}
