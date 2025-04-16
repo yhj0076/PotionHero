6f6912ae0910dd665b9e628a5a1cafe53ce9464d
King0wl <70518135+yhj0076@users.noreply.github.com>
2025-04-16 22:12:51 +0900

    Create Program.cs
    통신 연결 테스트 완료

---
+++ DummyClient/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using DummyClient.Session;
+using ServerCore;
+
+namespace DummyClient;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // DNS(Domain Name System)
+        string host = Dns.GetHostName();
+        IPHostEntry ipEntry = Dns.GetHostEntry(host);
+        IPAddress ipAddress = ipEntry.AddressList[0];
+        IPEndPoint endPoint = new IPEndPoint(ipAddress, 8080);
+        
+        Connector connector = new Connector();
+        connector.Connect(endPoint, () =>
+        {
+            return SessionManager.Instance.Generate();
+        },10);
+
+        while (true)
+        {
+            try
+            {
+                SessionManager.Instance.SendForEach();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            
+            Thread.Sleep(250);
+        }
+    }
+}
