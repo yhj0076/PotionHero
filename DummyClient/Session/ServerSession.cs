f0ac7a2a393fa2c6696756ee456cb0ca729cbea9
King0wl <70518135+yhj0076@users.noreply.github.com>
2025-04-16 22:12:31 +0900

    Create ServerSession.cs

---
+++ DummyClient/Session/ServerSession.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using ServerCore;
+
+namespace DummyClient.Session;
+
+public class ServerSession : PacketSession
+{
+    public override void OnConnected(EndPoint endPoint)
+    {
+        Console.WriteLine($"Connected to {endPoint}");
+    }
+
+    public override void OnSend(int numOfBytes)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void OnDisconnected(EndPoint endPoint)
+    {
+        Console.WriteLine($"Disconnected from {endPoint}");
+    }
+
+    public override void OnRecvPacket(ArraySegment<byte> buffer)
+    {
+        PacketManager.Instance.OnRecvPacket(this, buffer);
+    }
+}
