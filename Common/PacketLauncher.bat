fafaf17d6574cd1d26a084fa1065f0450e32af09
King0wl <70518135+yhj0076@users.noreply.github.com>
2025-04-16 17:46:32 +0900

    Update PacketLauncher.bat
    ServerPacketManager.cs 복사할 수 있도록 변경

--- Common/PacketLauncher.bat
+++ Common/PacketLauncher.bat
@@ -1,2 +1,4 @@
 START ../PacketGenerator/bin/net9.0/PacketGenerator.exe ../PacketGenerator/PacketLauncher.xml
 XCOPY /Y GenPackets.cs "../Server/Packet"
+
+XCOPY /Y ServerPacketManager.cs "../Server/Packet"
