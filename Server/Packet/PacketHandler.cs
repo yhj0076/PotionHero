80e3c86f7ad91ba8aa7d6a9f7cf586296bb95b5e
King0wl <70518135+yhj0076@users.noreply.github.com>
2025-04-16 17:24:54 +0900

    Create PacketHandler.cs

---
+++ Server/Packet/PacketHandler.cs
@@ -0,0 +1,21 @@
+﻿using Server;
+using Server.Session;
+using ServerCore;
+
+namespace PotionHeroServer.Packet;
+
+public class PacketHandler
+{
+    public static void C_GainedDmgHandler(PacketSession session, IPacket packet)
+    {
+        ClientSession clientSession = (ClientSession)session;
+
+        if (clientSession.Room == null)
+            return;
+        
+        GameRoom room = clientSession.Room;
+        room.Push(
+            () => room.Leave(clientSession)
+            );
+    }
+}
